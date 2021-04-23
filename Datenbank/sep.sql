f286e8cdcc8f61ec56830d348da852a15c3f2332
Georg Orfali <georg.orfali@stud.uni-due.de>
2021-04-23 22:18:09 +0200

    SEP Datenbakdiagramm und DB update

--- Datenbank/sep.sql
+++ Datenbank/sep.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 22. Apr 2021 um 20:11
+-- Erstellungszeit: 23. Apr 2021 um 22:16
 -- Server-Version: 10.4.11-MariaDB
 -- PHP-Version: 7.2.31
 
@@ -24,31 +24,133 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Tabellenstruktur für Tabelle `lehrende`
 --
 
-CREATE TABLE `user` (
-  `ID` int(11) NOT NULL,
-  `Matrikelnummer` int(7) NOT NULL,
-  `Password` varchar(255) NOT NULL
+CREATE TABLE `lehrende` (
+  `ID` int(255) NOT NULL,
+  `Nutzer_ID` int(255) NOT NULL,
+  `Rolle_ID` int(255) NOT NULL,
+  `Lehrstuhl` varchar(255) NOT NULL,
+  `Forschungsgebiet` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Daten für Tabelle `user`
+-- Tabellenstruktur für Tabelle `lehrveranstaltung`
 --
 
-INSERT INTO `user` (`ID`, `Matrikelnummer`, `Password`) VALUES
-(1, 3093800, 'test'),
-(2, 3093810, 'y');
+CREATE TABLE `lehrveranstaltung` (
+  `ID` int(255) NOT NULL,
+  `Veranstaltungs_ID` int(255) NOT NULL,
+  `Titel` varchar(255) NOT NULL,
+  `Semester` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `nutzer`
+--
+
+CREATE TABLE `nutzer` (
+  `ID` int(255) NOT NULL,
+  `Vorname` varchar(255) NOT NULL,
+  `Nachname` varchar(255) NOT NULL,
+  `Profilbild` varchar(255) NOT NULL,
+  `Adresse` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Passwort` varchar(255) NOT NULL,
+  `Rolle_ID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `rolle`
+--
+
+CREATE TABLE `rolle` (
+  `ID` int(255) NOT NULL,
+  `Rolle` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `student`
+--
+
+CREATE TABLE `student` (
+  `ID` int(255) NOT NULL,
+  `Nutzer_ID` int(255) NOT NULL,
+  `Rolle_ID` int(255) NOT NULL,
+  `Matrikelnummer` int(7) NOT NULL,
+  `Studienfach` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `teilnehmer`
+--
+
+CREATE TABLE `teilnehmer` (
+  `ID` int(255) NOT NULL,
+  `Lehrveranstaltungs_ID` int(255) NOT NULL,
+  `Nutzer_ID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `veranstaltung`
+--
+
+CREATE TABLE `veranstaltung` (
+  `ID` int(255) NOT NULL,
+  `Typ` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indizes der exportierten Tabellen
 --
 
 --
--- Indizes für die Tabelle `user`
+-- Indizes für die Tabelle `lehrende`
 --
-ALTER TABLE `user`
+ALTER TABLE `lehrende`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indizes für die Tabelle `lehrveranstaltung`
+--
+ALTER TABLE `lehrveranstaltung`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indizes für die Tabelle `nutzer`
+--
+ALTER TABLE `nutzer`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indizes für die Tabelle `rolle`
+--
+ALTER TABLE `rolle`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indizes für die Tabelle `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indizes für die Tabelle `teilnehmer`
+--
+ALTER TABLE `teilnehmer`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -56,10 +158,40 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT für Tabelle `user`
+-- AUTO_INCREMENT für Tabelle `lehrende`
 --
-ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `lehrende`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `lehrveranstaltung`
+--
+ALTER TABLE `lehrveranstaltung`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `nutzer`
+--
+ALTER TABLE `nutzer`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `rolle`
+--
+ALTER TABLE `rolle`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `student`
+--
+ALTER TABLE `student`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `teilnehmer`
+--
+ALTER TABLE `teilnehmer`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
