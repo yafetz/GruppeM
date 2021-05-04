a67a63cadf48ef0d3278a5978b0ae601d62bce90
Georg Orfali <georg.orfali@stud.uni-due.de>
2021-05-04 12:40:00 +0200

    Changing Master

--- Server/src/main/java/com/example/demo12/repository/LehrveranstaltungRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo12.repository;
-
-import com.example.demo12.entities.Lehrveranstaltung;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LehrveranstaltungRepository extends JpaRepository<Lehrveranstaltung,Integer> {
-}
--- Server2/src/main/java/Server/ServerApplication.java
+++ Server2/src/main/java/Server/ServerApplication.java
@@ -1,14 +1,12 @@
-package com.example.demo12;
+package Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo12Application {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(Demo12Application.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
