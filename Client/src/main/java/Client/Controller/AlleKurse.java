b9132bc895bf2ca124c6fe8810d15d6cbac26b3f
Serena Böker <serena.boeker@stud.uni-due.de>
2021-05-05 14:55:44 +0200

    AlleKurse Controller Clientside

--- Client/src/main/java/Client/Controller/AlleKurse.java
+++ Client/src/main/java/Client/Controller/AlleKurse.java
@@ -1,5 +1,34 @@
 package Client.Controller;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class AlleKurse {
 
+    @FXML
+    private TableView alleLv;
+
+
+    public void initialize() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().
+                uri(URI.create("http://localhost:8080/lehrveranstaltungen/all")).build();
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
