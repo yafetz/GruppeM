7556113612d290c32c176357ac6c90c133a6957a
Georg Orfali <georg.orfali@stud.uni-due.de>
2021-05-05 12:48:53 +0200

    Adding Login Server Side

--- Server/src/main/java/Server/Controller/Login.java
+++ Server/src/main/java/Server/Controller/Login.java
@@ -1,22 +1,16 @@
 package Server.Controller;
 
+import Server.Modell.Lehrender;
 import Server.Modell.Nutzer;
 import Server.Modell.Student;
+import Server.Repository.LehrenderRepository;
 import Server.Repository.NutzerRepository;
 import Server.Repository.StudentRepository;
-import Server.Services.StudentService;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/login")
@@ -25,14 +19,55 @@ public class Login {
     NutzerRepository nutzerRepository;
     @Autowired
     StudentRepository studentRepository;
-    @GetMapping("/{matrikelnummer}&{passwort}")
-    public Nutzer getStudent(@PathVariable int matrikelnummer, @PathVariable String email, @PathVariable String passwort){
-        Student student = studentRepository.findStudentByMatrikelnummer(matrikelnummer);
-        if(student == null){
-            Nutzer nutzer = nutzerRepository.findNutzerByEmail(email);
-        }else{
-
+    @Autowired
+    LehrenderRepository lehrenderRepository;
+    @GetMapping("/{user}&{passwort}")
+    public Object getLogin(@PathVariable String user, @PathVariable String passwort){
+        Lehrender lehrender = new Lehrender();
+        Student student = new Student();
+        if(user.contains("@")){
+            Nutzer nutzer = nutzerRepository.findNutzerByEmail(user);
+            //check if nutzer is found
+            if(nutzer != null){
+                //check if Lehrender or Student
+                student = studentRepository.findStudentByNutzerId(nutzer);
+                lehrender = lehrenderRepository.findLehrenderByNutzerId(nutzer);
+                if(student == null && lehrender != null){
+                    //check if password is correct
+                    if(checkPassword(lehrender,passwort)){
+                        return lehrender;
+                    }
+                }else if(lehrender == null && student != null){
+                    if(checkPassword(student,passwort)){
+                        return student;
+                    }
+                }
+            }
+        }else if(user.matches("[0-9]+") && user.length() == 7){
+            student = studentRepository.findStudentByMatrikelnummer(Integer.valueOf(user));
+            if(student != null){
+                if(checkPassword(student,passwort)){
+                    return student;
+                }
+            }
         }
-        return student.getNutzer_id();
+        return null;
+    }
+
+    public boolean checkPassword(Object object, String passwort){
+        if(object instanceof Student){
+           if( ((Student) object).getNutzerId().getPasswort().equals(passwort) ){
+               return true;
+           }else{
+               return false;
+           }
+        }else if( object instanceof Lehrender){
+            if( ((Lehrender) object).getNutzerId().getPasswort().equals(passwort) ){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return false;
     }
 }
--- Server/src/main/java/Server/Repository/StudentRepository.java
+++ Server/src/main/java/Server/Repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package Server.Repository;
 
+import Server.Modell.Nutzer;
 import Server.Modell.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
     Student findStudentByMatrikelnummer(Integer matrikelnummer);
+    Student findStudentByNutzerId(Nutzer nutzer);
 }
