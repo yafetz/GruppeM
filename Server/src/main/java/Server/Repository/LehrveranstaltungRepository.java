c42e8f21a670ba52a03f960083fc3597dd3d5ef6
Serena Böker <serena.boeker@stud.uni-due.de>
2021-05-05 14:00:24 +0200

    Lehrveranstaltung hinzugefügt

---
+++ Server/src/main/java/Server/Repository/LehrveranstaltungRepository.java
@@ -0,0 +1,8 @@
+package Server.Repository;
+
+import Server.Modell.Lehrender;
+import Server.Modell.Lehrveranstaltung;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LehrveranstaltungRepository extends JpaRepository<Lehrveranstaltung,Long> {
+}
