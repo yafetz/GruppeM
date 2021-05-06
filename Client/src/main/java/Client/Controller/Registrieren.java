95ddfe21d2f268fefb6b16e50c55ea15953cf85b
Georg Orfali <georg.orfali@stud.uni-due.de>
2021-05-06 23:04:27 +0200

    bug fix

--- Client/src/main/java/Client/Controller/Registrieren.java
+++ Client/src/main/java/Client/Controller/Registrieren.java
@@ -1,21 +1,18 @@
 package Client.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
-import java.awt.event.ActionEvent;
 
 public class Registrieren {
     @FXML
     private TextField email;
     @FXML
     private Button registrierbutton;
-    @FXML
-    public void registrieren (ActionEvent Event){
+
+
+    public void registrieren(ActionEvent Event){
         System.out.println(email.getText());
     }
-
-
-
 }
