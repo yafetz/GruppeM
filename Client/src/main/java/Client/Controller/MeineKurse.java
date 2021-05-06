9b24d81f230af13d061bdb9a7cd4204e959b53bc
Serena Böker <serena.boeker@stud.uni-due.de>
2021-05-07 00:14:12 +0200

    added links between meineKurse, alleKurse, userprofile

--- Client/src/main/java/Client/Controller/MeineKurse.java
+++ Client/src/main/java/Client/Controller/MeineKurse.java
@@ -5,11 +5,19 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URI;
@@ -32,6 +40,14 @@ public class MeineKurse implements Initializable {
     private TableColumn col_LvArt;
     @FXML
     private TableColumn col_LvLehrende;
+    @FXML
+    private Button meineKurse;
+    @FXML
+    private Button alleKurse;
+    @FXML
+    public ImageView profilBild;
+    @FXML
+    public Hyperlink namenLink;
 
 
     @Override
@@ -55,6 +71,63 @@ public class MeineKurse implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void meineKurseAufrufen(ActionEvent event) {
+        event.consume();
+        Stage stage = (Stage) alleKurse.getScene().getWindow();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("meineKurse.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+            MeineKurse meineKurse = loader.getController();
+            Scene scene = new Scene(root);
+            String homescreencss = getClass().getClassLoader().getResource("css/login.css").toExternalForm();
+            scene.getStylesheets().add(homescreencss);
+            stage.setScene(scene);
+            stage.setMaximized(false);
+            stage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void alleKurseAufrufen(ActionEvent event) {
+        event.consume();
+        Stage stage = (Stage) alleKurse.getScene().getWindow();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("alleKurse.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+            AlleKurse alleKurse = loader.getController();
+            Scene scene = new Scene(root);
+            String homescreencss = getClass().getClassLoader().getResource("css/login.css").toExternalForm();
+            scene.getStylesheets().add(homescreencss);
+            stage.setScene(scene);
+            stage.setMaximized(false);
+            stage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void eigeneProfilSeiteAufrufen(ActionEvent event) {
+        event.consume();
+        Stage stage = (Stage) namenLink.getScene().getWindow();
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("userprofile.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+            Userprofil userprofil = loader.getController();
+            Scene scene = new Scene(root);
+            String homescreencss = getClass().getClassLoader().getResource("css/login.css").toExternalForm();
+            scene.getStylesheets().add(homescreencss);
+            stage.setScene(scene);
+            stage.setMaximized(false);
+            stage.show();
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
