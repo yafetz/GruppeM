c61e65e942fc57a59af2c1bfc17501e7be91d7a4
Nguyen <lam.nguyen-le@stud.uni-due.de>
2021-05-07 12:00:33 +0200

    änderungen an userprofil

--- Client/src/main/java/Client/Controller/Userprofil.java
+++ Client/src/main/java/Client/Controller/Userprofil.java
@@ -1,6 +1,7 @@
 package Client.Controller;
 
 import Client.Modell.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -11,6 +12,18 @@ public class Userprofil {
     private Label mailadresse;
     @FXML
     private Label matr;
+    @FXML
+    private Label plz;
+    @FXML
+    private Label adresse;
+    @FXML
+    private Label lehrstuhl;
+    @FXML
+    private Label forschungsgebiet;
+    @FXML
+    private Label city;
+    @FXML
+    private Label number;
 
     private Student student;
     private Lehrender lehrender;
@@ -24,6 +37,11 @@ public class Userprofil {
          username.setText(student.getNutzer().getVorname() + " " + student.getNutzer().getNachname());
          mailadresse.setText(student.getNutzer().getEmail());
          matr.setText(String.valueOf(student.getMatrikelnummer()));
+         plz.setText(String.valueOf(student.getNutzer().getPlz()));
+         adresse.setText(student.getNutzer().getStrasse());
+         number.setText(String.valueOf(student.getNutzer().getHausnummer()));
+         lehrstuhl.setVisible(false);
+         forschungsgebiet.setVisible(false);
 
      }
     public void setLehrender(Lehrender lehrender) {
@@ -31,9 +49,18 @@ public class Userprofil {
         username.setText(lehrender.getNutzer().getVorname() + " " + lehrender.getNutzer().getNachname());
         mailadresse.setText(lehrender.getNutzer().getEmail());
         matr.setVisible(false);
+        plz.setText(String.valueOf(lehrender.getNutzer().getPlz()));
+        adresse.setText(lehrender.getNutzer().getStrasse());
+        number.setText(String.valueOf(lehrender.getNutzer().getHausnummer()));
+        lehrstuhl.setText(lehrender.getLehrstuhl());
+        forschungsgebiet.setText(lehrender.getForschungsgebiet());
 
     }
 
 
+    public void meineKurseAufrufen(ActionEvent actionEvent) {
+    }
 
+    public void alleKurseAufrufen(ActionEvent actionEvent) {
+    }
 }
