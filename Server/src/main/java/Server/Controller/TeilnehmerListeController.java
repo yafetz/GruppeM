aed0d043e343aa007420a9b3cbb68d49e0bd7727
Yafet Zehaie <yafet.zehaie@stud.uni-due.de>
2021-05-17 16:15:00 +0200

    Modell update, Controller update

--- Server/src/main/java/Server/Controller/TeilnehmerListeController.java
+++ Server/src/main/java/Server/Controller/TeilnehmerListeController.java
@@ -1,12 +1,11 @@
 package Server.Controller;
 
 
+import Server.Modell.Lehrveranstaltung;
+import Server.Modell.Nutzer;
 import Server.Modell.Student;
 import Server.Modell.TeilnehmerListe;
-import Server.Repository.LehrenderRepository;
-import Server.Repository.LehrveranstaltungRepository;
-import Server.Repository.StudentRepository;
-import Server.Repository.TeilnehmerListeRepository;
+import Server.Repository.*;
 import Server.Services.TeilnehmerListeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,17 +20,19 @@ import java.util.List;
 public class TeilnehmerListeController {
     StudentRepository studentRepository;
     LehrveranstaltungRepository lehrveranstaltungRepository;
+    NutzerRepository nutzerRepository;
     LehrenderRepository lehrenderRepository;
     TeilnehmerListeService teilnehmerListeService;
     @Autowired
     TeilnehmerListeRepository teilnehmerListeRepository;
 
 
-    public TeilnehmerListeController(StudentRepository studentRepository, LehrveranstaltungRepository lehrveranstaltungRepository, LehrenderRepository lehrenderRepository, TeilnehmerListeService teilnehmerListeService) {
+    public TeilnehmerListeController(StudentRepository studentRepository, LehrveranstaltungRepository lehrveranstaltungRepository,NutzerRepository nutzerRepository ,LehrenderRepository lehrenderRepository, TeilnehmerListeService teilnehmerListeService) {
         this.studentRepository = studentRepository;
         this.lehrveranstaltungRepository = lehrveranstaltungRepository;
         this.lehrenderRepository = lehrenderRepository;
         this.teilnehmerListeService = teilnehmerListeService;
+        this.nutzerRepository= nutzerRepository;
 
     }
     @GetMapping("/{lehrveranstaltungsId}")
@@ -41,7 +42,13 @@ public class TeilnehmerListeController {
         return teilnehmer;
     }
 
+    /*@GetMapping("/{lehrveranstaltungsId/{nutzerId}")
+    public boolean isNutzer(@PathVariable long lehrveranstaltungsId, @PathVariable long nutzerId){
+        Nutzer nutzer = nutzerRepository.findNutzerById(nutzerId);
+        Lehrveranstaltung lehrveranstaltung = lehrveranstaltungRepository.findLehrveranstaltungById(lehrveranstaltungsId);
+        return teilnehmerListeRepository.findAllByLehrveranstaltungAndIdExists(lehrveranstaltung, nutzerId);
 
+    }*/
 
 
 
