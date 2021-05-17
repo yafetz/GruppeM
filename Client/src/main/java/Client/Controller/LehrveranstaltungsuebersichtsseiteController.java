8b62a24738f8b41b7b7154a04bef55b359ad3661
Nguyen <lam.nguyen-le@stud.uni-due.de>
2021-05-17 19:12:37 +0200

    lehrveranstaltungsuebersichtsseite änderungen

--- Client/src/main/java/Client/Controller/LehrveranstaltungsuebersichtsseiteController.java
+++ Client/src/main/java/Client/Controller/LehrveranstaltungsuebersichtsseiteController.java
@@ -1,5 +1,6 @@
 package Client.Controller;
 
+import Client.Layouts.Layout;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,34 +18,32 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LehrveranstaltungsuebersichtsseiteController {
-@FXML
+    @FXML
     private Label title;
-@FXML
+    @FXML
     private Button materialUpload;
+
+
+    private Object nutzer;
+
 
     public void initialize() {
 
     }
 
 
-
-@FXML
-    private void uploadPressedButton(ActionEvent event) {
-        event.consume();
-        Stage stage = (Stage) alle.getScene().getWindow();
+    @FXML
+    private void materialUploadPressedButton(ActionEvent event) {
+        Stage stage = (Stage) materialUpload.getScene().getWindow();
+        Layout homeScreen = null;
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getClassLoader().getResource("alleKurse.fxml"));
-            AnchorPane root = (AnchorPane) loader.load();
-            AlleKurseController alleKurseController = loader.getController();
-            alleKurseController.setNutzerInstanz(nutzerInstanz);
-            Scene scene = new Scene(root);
-            String homescreencss = getClass().getClassLoader().getResource("css/login.css").toExternalForm();
-            scene.getStylesheets().add(homescreencss);
-            stage.setScene(scene);
-            stage.setMaximized(false);
-            stage.show();
-        } catch (IOException e){
+            homeScreen = new Layout("lehrmaterialUpload.fxml", stage);
+            if (homeScreen.getController() instanceof HomescreenController) {
+                ((HomescreenController) homeScreen.getController()).setNutzerInstanz(nutzer);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
+}
