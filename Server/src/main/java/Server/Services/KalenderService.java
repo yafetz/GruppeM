d1d6ff65695c92e668c968ccecc371b4083f6e46
Natalie <natalie.schmidt@stud.uni-due.de>
2021-06-25 00:48:04 +0200

    Kalender Email reminder

--- Server/src/main/java/Server/Services/KalenderService.java
+++ Server/src/main/java/Server/Services/KalenderService.java
@@ -1,7 +1,25 @@
 package Server.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class KalenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendEmail(String email, String Reminder){
+        SimpleMailMessage mailMessage= new SimpleMailMessage();
+        mailMessage.setFrom("projektsep@gmail.com");
+        mailMessage.setTo(email);
+        mailMessage.setText(Reminder);
+        mailMessage.setSubject("Sie haben einen Termin!");
+
+
+        mailSender.send(mailMessage);
+        System.out.println("Du hast ein Termin!");
+    }
 }
