8a6df5c6d51af629a140237f1406726c1f890fcd
Georg Orfali <georg.orfali@stud.uni-due.de>
2021-07-20 12:56:56 +0200

    Bug fix

--- Client/src/main/java/Client/Controller/Thema/LiteraturHinzufügenController.java
+++ Client/src/main/java/Client/Controller/Thema/LiteraturHinzufügenController.java
@@ -43,6 +43,7 @@ public class LiteraturHinzufügenController {
     public void setThema(Thema thema){
         this.thema = thema;
         thema_titel.setText(thema.getTitel());
+        //Load Tables
     }
 
     public Layout getLayout() {
@@ -61,4 +62,42 @@ public class LiteraturHinzufügenController {
         ((LiteraturUploadController) layout.getController()).setLayout(layout);
         ((LiteraturUploadController) layout.getController()).setThema(thema);
     }
+
+    public void populate_noch_vorhanden_literatur() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/projektgruppe/" + thema.getId() + "/Mitglieder")).build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            ObjectMapper mapper = new ObjectMapper();
+            List<Student> students = mapper.readValue(response.body(), new TypeReference<List<Student>>() {});
+
+            literatur_noch_vorhanden_titel_column.setCellValueFactory(new PropertyValueFactory<Student, String>("NachnameVorname"));
+
+            ObservableList<Student> obsStud = FXCollections.observableList(students);
+            Literatur_noch_vorhanden_table.setItems(obsStud);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void populateNeueTableView() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/projektgruppe/" + thema.getId() + "/moeglicheMitglieder/")).build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            ObjectMapper mapper = new ObjectMapper();
+            List<Student> students = mapper.readValue(response.body(), new TypeReference<List<Student>>() {});
+
+            literatur_jetzt_table.setEditable(true);
+            literatur_jetzt_titel_column.setCellValueFactory(new PropertyValueFactory<Student, String>("NachnameVorname"));
+
+            ObservableList<Student> obsStud = FXCollections.observableList(students);
+            literatur_jetzt_table.setItems(obsStud);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
